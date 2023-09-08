aa1c3437da3aaf7167e1a2a20851e9d00c153424
arun_microexcel <Arun.K@microexcel.com>
2023-09-08 17:15:11 +0530

    ad

--- controllers/bus.controller.js
+++ controllers/bus.controller.js
@@ -12,38 +12,38 @@ module.exports.busDetails = async (req, res, next) => {
 }
 
 
-module.exports.busDetailsById = async (req, res, next) => {   
-    // if(!ObjectId.isValid (req.params.id)) 
-    // return res.status(400).send(`No record found with given id: ${req.params.id}`)
-    // var id = req.params.id;  
-    // var o_id = new ObjectId(id);
-    // const findResult = await db.collection("bus").find({_id:o_id}).toArray();
-    // res.send(findResult);  
+// module.exports.busDetailsById = async (req, res, next) => {   
+//     // if(!ObjectId.isValid (req.params.id)) 
+//     // return res.status(400).send(`No record found with given id: ${req.params.id}`)
+//     // var id = req.params.id;  
+//     // var o_id = new ObjectId(id);
+//     // const findResult = await db.collection("bus").find({_id:o_id}).toArray();
+//     // res.send(findResult);  
 
-    if(!ObjectId.isValid (req.params.id)) 
-    return res.status(400).send(`No record found with given id: ${req.params.id}`)
+//     if(!ObjectId.isValid (req.params.id)) 
+//     return res.status(400).send(`No record found with given id: ${req.params.id}`)
 
-    Buses.findById(req.params.id, (err, docs) => {
-        if(!err) {res.send(docs);}
-        else {console.log('Error' + Json.stringfy(err, undefined, 2)); }
-    });
-}
+//     Buses.findById(req.params.id, (err, docs) => {
+//         if(!err) {res.send(docs);}
+//         else {console.log('Error' + Json.stringfy(err, undefined, 2)); }
+//     });
+// }
  
-module.exports.busSearch = async (req, res, next) => { 
-    const { from, to , category, type } = req.query; 
+// module.exports.busSearch = async (req, res, next) => { 
+//     const { from, to , category, type } = req.query; 
    
-    // db.collection("bus").find({ from, to , category, type}).toArray(function(e, d) {
-    //     if(!e) {res.send(d);}
-    //     else {console.log('Error' + Json.stringfy(err, undefined, 2)); } 
-    // }); 
+//     // db.collection("bus").find({ from, to , category, type}).toArray(function(e, d) {
+//     //     if(!e) {res.send(d);}
+//     //     else {console.log('Error' + Json.stringfy(err, undefined, 2)); } 
+//     // }); 
 
-    Buses.find({ from, to , category, type}).toArray(function(e, d) {
-        if(!e) {res.send(d);}
-        else {console.log('Error' + Json.stringfy(err, undefined, 2)); } 
-    }); 
+//     Buses.find({ from, to , category, type}).toArray(function(e, d) {
+//         if(!e) {res.send(d);}
+//         else {console.log('Error' + Json.stringfy(err, undefined, 2)); } 
+//     }); 
     
     
-} 
+// } 
 
 
 module.exports.placesDetails = async (req, res, next) => {   
