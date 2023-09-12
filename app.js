bb6086cc06556236866e02b943cf90ae4a7c5a2b
arun_microexcel <Arun.K@microexcel.com>
2023-09-12 14:47:58 +0530

    ad

--- app.js
+++ app.js
@@ -21,7 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: ['https://shy-erin-penguin-wrap.cyclic.cloud/api']
+}));
 app.use(passport.initialize());  
 app.use('/api', rtsIndex);
 app.set('view engine', 'ejs')
