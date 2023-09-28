8f74d06ace7c1c63fdbeab436dc603f630b3fcb8
arun_microexcel <Arun.K@microexcel.com>
2023-09-28 13:56:26 +0530

    add

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,8 +20,8 @@ let transporter = nodemailer.createTransport({
     port: 465, 
     secure: true,
     auth: {
-        user: process.env.AUTH_EMAIL,
-        pass: process.env.AUTH_PASS,
+        user: 'arun70840@gmail.com',
+        pass: 'nzitmddyckrepiux',
     }, 
     tls: {
         // do not fail on invalid certs
