6201051e3862218327cfae2855a71a0d58117392
arun_microexcel <Arun.K@microexcel.com>
2023-10-03 12:29:06 +0530

    adad

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -31,6 +31,156 @@ module.exports.booking = (req, res, next) => {
     booking.save((err, doc) => {
         if (!err) {
             res.send(doc); 
+            var mailOptions = {
+                from: 'arun70840@gmail.com',
+                to: 'arun70840@gmail.com',
+                subject: 'My Travels',
+                html: `  <table width="600px" style="border-collapse: collapse; font-family: 'Bai Jamjuree', sans-serif;  margin: auto;overflow: hidden; border: 1px solid #f7f7f7;"> 
+                <tr>
+                    <td align="center" colspan="2" style=" font-size: 25px; background-color: #0d61b7;height: 100px; color:#fff;text-align: center;">
+                          Confirmed !
+                    </td>
+                </tr> 
+                <tr>
+                    <td align="left" colspan="2" style="padding:15px 30px;"> 
+                         
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" colspan="2" style="font-size: 20px; padding:10px 30px;">
+                        Your booking is confirmed on ` + bookedDate + `
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" colspan="2" style="font-size:14px; padding:0px 30px;"> 
+                        Booking ID # ` + bookingID + `
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" colspan="2" style="font-size:14px; padding:25px 30px;">
+                        <table style="width: 100%;   border-collapse: collapse;">
+                            <tr style="font-weight: bold;">
+                                <td>From</td>
+                                <td>To</td>
+                                <td>Date</td>
+                                <td>Departure Time</td>
+                            </tr>
+                            <tr>
+                                <td>` + busFrom + `</td>
+                                <td>` + busTo + `</td>
+                                <td>` + bookedDate + `</td>
+                                <td>` + busTimeFrom + `</td>
+                            </tr> 
+                            <tr>
+                                <td align="left"  style="padding:10px 30px;"> 
+                                     
+                                </td>
+                            </tr>
+                            <tr style="font-weight: bold;">
+                                <td>Arrival Time</td>
+                                <td>Journey Hours</td>
+                                <td>No. of Seats</td> 
+                                <td>Seat Type</td> 
+                            </tr>
+                            <tr>
+                                <td>` + busTimeTo + `</td>
+                                <td>` + busTravelTime + `</td>
+                                <td>` + totalSeat + `</td> 
+                                <td>` + seatType + `</td>
+
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" colspan="2" style="font-size:14px; padding:5px 30px;">
+                        <table style="width: 100%;   border-collapse: collapse;">
+                            <tr style="font-weight: bold;">
+                                <td>Pick Up Point</td> 
+                            </tr>
+                            <tr>
+                                <td>` + pickup + `</td> 
+                            </tr> 
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" colspan="2" style="padding:15px 30px;"> 
+                        <hr style="border-top: 1px solid #f7f7f7;"> 
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" colspan="2" style="font-size:14px; padding:0px 30px;font-weight: bold;"> 
+                        Traveller details
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" colspan="2" style="font-size:14px; padding:5px 30px;">
+                        <table style="width: 100%;   border-collapse: collapse;">
+                            <tr style="font-weight: bold;">
+                                <td>Sl.No.</td>
+                                <td>Name</td>
+                                <td>Age</td>
+                                <td>Gender</td>
+                                <td>Seat No.</td>
+                            </tr>
+                            <tr>
+                                <td>01</td>
+                                <td>Xyz</td>
+                                <td>25</td>
+                                <td>Male</td>
+                                <td>5c</td>
+                            </tr> 
+                            <tr>
+                                <td>01</td>
+                                <td>Xyz</td>
+                                <td>25</td>
+                                <td>Male</td>
+                                <td>5c</td>
+                            </tr> 
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" colspan="2" style="padding:15px 30px;"> 
+                        <hr style="border-top: 1px solid #f7f7f7;"> 
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" colspan="2" style="font-size:14px; padding:0px 30px;font-weight: bold;"> 
+                        Payment details
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" colspan="2" style="font-size:14px; padding:5px 30px;">
+                        <table style="width: 100%;    border-collapse: collapse;">
+                            <tr >
+                                <td style="font-weight: bold;">key</td>
+                                <td style="float: right;">value</td> 
+                            </tr>
+                            <tr>
+                                <td  style="font-weight: bold;">key</td>
+                                <td  style="float: right;">value</td> 
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="left" colspan="2" style="padding:15px 30px;"> 
+                         
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" colspan="2" style=" font-size: 14px; background-color: #0d61b7;height: 40px; color:#fff;text-align: center;">
+                        copyrights @2023 , All rights reserved
+                    </td>
+                </tr> 
+            </table> `,
+            };
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error)
+                    console.log(error);
+            })
         }
         else {
             return next(err);
