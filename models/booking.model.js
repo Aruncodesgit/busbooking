c7166088025a7bf746167878352468430486b0fd
arun_microexcel <Arun.K@microexcel.com>
2023-10-17 15:40:32 +0530

    adda

--- models/booking.model.js
+++ models/booking.model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');  
 
 const fareSchema = new mongoose.Schema({
-    perhead :  Number,
-    subTotal :  Number,
+    pricePerHead :  Number,
+    subtotal :  Number,
     gst :  Number,
     discountVal :  Number,
-    discountedAmt :  Number,
-    promoCodeAmount :  Number,
+    dicountedAmt  :  Number,
+    promocode :  Number,
     totalFare :  Number
 });
 
